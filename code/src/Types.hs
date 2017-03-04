a4b7cb4c18c69571e24c388ada7123e6164bbcbf
MarkWRuszczycky <mwruszczycky@gmail.com>
2017-03-04 10:00:26 -0600

    Refactors Types.hs.

--- code/src/Types.hs
+++ code/src/Types.hs
@@ -2,13 +2,28 @@ module Types
     ( module Types
     ) where
 
-data Source a = StdIn | File a deriving (Eq)
+data Source a =
+    StdIn
+  | File a
+  deriving (Eq)
 
-data Mode = HelpMode | ShuffleMode | UnshuffleMode | PrintMode deriving (Eq)
+data Mode =
+    HelpMode
+  | ShuffleMode
+  | UnshuffleMode
+  | PrintMode
+  deriving (Eq)
 
-data Base = NoBase | Dec | Hex deriving (Eq)
+data Base =
+    NoBase
+  | Dec
+  | Hex
+  deriving (Eq)
 
-data EitherArgs a b = BadArgs a | GoodArgs b deriving (Eq)
+data EitherArgs a b =
+    BadArgs a
+  | GoodArgs b
+  deriving (Eq)
 
 data Cmds = Cmds
     { source :: Source String
